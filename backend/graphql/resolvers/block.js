0764155c883a4ceba4333db74f229674352a964f
mhsuab <b05901003@ntu.edu.tw>
2021-01-17 13:15:05 +0800

    ✔️ DONE resolvers

--- backend/graphql/resolvers/block.js
+++ backend/graphql/resolvers/block.js
@@ -15,7 +15,7 @@ module.exports = {
             // NOTE: return true if the `date` has blocks
             const { userID } = checkAuth(request);
             const ifExist = await Block.exists({
-                $and: [{ userID }, { "Day": date }]
+                $and: [{ userID }, { "Day": date }, { "onCalendar": true }]
             }).catch(err => {
                 throw new Error(err);
             })
@@ -23,7 +23,7 @@ module.exports = {
         }
     },
     Mutation: {
-        addBlock: async (_, { block }, { request }) => {
+        addBlock: async (_, { block }, { request, pubsub }) => {
             const { userID, calendarExpiresDay } = checkAuth(request);
             const addBlock = await Block.create({
                 userID,
@@ -32,9 +32,23 @@ module.exports = {
             }).catch(err => {
                 throw new Error(err);
             })
+            if (addBlock.onCalendar) {
+                await (pubsub.publish('calendar', {
+                    updateCalendar: {
+                        type: 'ADDED',
+                        info: {
+                            "Day": addBlock.Day,
+                            "ifExist": await Block.exists({
+                                $and: [{ userID }, { "Day": addBlock.Day }, { "onCalendar": true }]
+                            })
+                        }
+                    }
+                }))
+            }
+
             return addBlock;
         },
-        deleteBlock: async (_, { blockID }, { request }) => {
+        deleteBlock: async (_, { blockID }, { request, pubsub }) => {
             const { userID } = checkAuth(request);
             const delBlock = await Block.findOneAndDelete({
                 $and: [{ userID }, { "_id": blockID }]
@@ -42,16 +56,31 @@ module.exports = {
                 throw new Error(err);
             });
             if (!delBlock) throw new Error("Unable to find target block to delete.");
+
+            if (delBlock.onCalendar) {
+                await (pubsub.publish('calendar', {
+                    updateCalendar: {
+                        type: 'DELETED',
+                        info: {
+                            "Day": delBlock.Day,
+                            "ifExist": await Block.exists({
+                                $and: [{ userID }, { "Day": delBlock.Day }, { "onCalendar": true }]
+                            })
+                        }
+                    }
+                }))
+            }
+
             return delBlock;
         },
-        updateBlock: async (_, { blockID, block }, { request }) => {
+        updateBlock: async (_, { blockID, block }, { request, pubsub }) => {
             const { userID } = checkAuth(request);
             const upBlock = await Block.findOneAndUpdate(
                 {
                     $and: [{ userID }, { "_id": blockID }]
                 }, {
-                    ...block
-                },
+                ...block
+            },
                 {
                     new: true,
                     useFindAndModify: false
@@ -60,6 +89,19 @@ module.exports = {
                 throw new Error(err);
             });
             if (!upBlock) throw new Error('Unablt to find block');
+
+            await (pubsub.publish('calendar', {
+                updateCalendar: {
+                    type: 'UPDATED',
+                    info: {
+                        "Day": upBlock.Day,
+                        "ifExist": await Block.exists({
+                            $and: [{ userID }, { "Day": upBlock.Day }, { "onCalendar": true }]
+                        })
+                    }
+                }
+            }))
+
             return upBlock;
         }
     },
