c837505b0439a99bb74e51c3170c05f43b63ae51
mhsuab <b05901003@ntu.edu.tw>
2021-01-20 16:37:59 +0800

    change title

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,4 +1,4 @@
-export const serviceTitle = 'Web is hard, but LIFE is harder';
+export const serviceTitle = '📆 Web is hard, but LIFE is harder';
 export const navBarColor = '#434c5e';
 export const sideBarColor = '#686f7e';
 export const mainBackground = '#e5e9f0';
