34118255ea95594ba89f236fcfaf05f7062fa2fa
ban9975 <s9960733@gmail.com>
2021-01-21 15:29:59 +0800

    calendar

--- frontend/src/components/Calendar.js
+++ frontend/src/components/Calendar.js
@@ -9,45 +9,28 @@ import moment from "moment"
 
 
 const Calendar = () => {
-    // const { user } = useContext(AuthContext)
-    // const [ activeDates, setActiveDates ] = useState([false])
-    // const thisMonth = moment(new Date).format("YYYY/MM");
-    // console.log(thisMonth) 
+    const { user } = useContext(AuthContext)
+    const [ events, setEvents ] = useState([])
+    const thisMonth = moment(new Date).format("YYYY/MM")
+    console.log(thisMonth) 
 
-    // const { loading, data, subscribeToMore, refetch } = useQuery(GET_MONTH_CALENDAR, {
-    //     variables: { month: thisMonth }
-    // })
+    const { loading, data, subscribeToMore, refetch } = useQuery(GET_MONTH_CALENDAR, {
+        variables: { month: thisMonth }
+    })
 
-    // useEffect(() => {
-    //     subscribeToMore({
-    //         document: UPDATE_CALENDAR,
-    //         updateQuery: (prev, { subscriptionData }) => {
-    //             console.log(subscriptionData)
-    //             if(!subscriptionData.data || !user) return prev
-    //             const newMsg = subscriptionData.data.updateCalendar
-    //             console.log(newMsg)
-    //             // if(subscriptionData.data.message.mutation === "CREATED"){
-    //             //     if((newMsg.sender === sender && newMsg.receiver === receiver) || (newMsg.receiver === sender && newMsg.sender === receiver)) {
-    //             //         setStatus({ type: 'success', msg: 'Message Sent'})
-    //             //         return { ...prev, messages: [...prev.messages, ...subscriptionData.data.message.messages] }
-    //             //     }
-    //             // }
-    //             // else if(subscriptionData.data.message.mutation === "DELETED") {
-    //             //     if((newMsg.sender === sender) || (newMsg.sender === receiver)) {
-    //             //         setStatus({ type: 'info', msg: 'Message cache cleared'})
-    //             //         return { ...prev, messages:[] }
-    //             //     }
-    //             // }
-    //             return prev
-    //         }
-    //     })
-    // }, [subscribeToMore, user])
+    useEffect(async () => {
+        const act = await refetch()
+        console.log(act.data.getMonth)
+        seetEvents(parseMonthData(act.data.getMonth))
+    }, [user])
 
-    // useEffect(async () => {
-    //     const act = await refetch()
-    //     console.log(act.data.getMonth)
-    //     setActiveDates[act.data.getMonth]
-    // }, [user])
+    const parseMonthData = (calendar_data) => {
+        const target = moment(new Date(thisMonth)).format("YYYY/MM/DD");
+        return [...Array(new Date(thisMonth.slice(0, 4), thisMonth.slice(-2), 0).getDate()).keys()].reduce((accumulator, day) => {
+            if (calendar_data[day]) return [...accumulator, { title: '', date: moment(target).add(day, 'days').format("YYYY-MM-DD") }];
+            else return accumulator
+        }, [])
+    }
 
     const CalendarStyle = {
         margin: "0vw",
@@ -72,62 +55,10 @@ const Calendar = () => {
                     center: "title",
                     right: ""
                 }}
-                events={[
-                    { title: '', date: '2021-01-18' },
-                    { title: '', date: '2021-01-21' },
-                ]}
+                events={events}
             />
         </div>
     )
 }
 
-
 export default Calendar
-
-// import React, { Component } from 'react'
-// import FullCalendar from '@fullcalendar/react'
-// import dayGridPlugin from '@fullcalendar/daygrid'
-// import interactionPlugin from "@fullcalendar/interaction" // needed
-
-
-// class Calendar extends Component {
-//     constructor(props) {
-//         super(props);
-//     }
-
-//     render() {
-//         const CalendarStyle = {
-//             margin: "0vw",
-//             background: "#FFFFFF",
-//             textAlign: "left",
-//             borderRadius: '3px',
-//             padding: '3px',
-//             border: '1px solid rgba(34, 36, 38, 0.15)'
-//         };
-
-//         return (
-//             <div style={CalendarStyle} className="Calendar">
-//                 <FullCalendar
-//                     height="100%"
-//                     aspectRatio="1"
-//                     plugins={[dayGridPlugin, interactionPlugin]}
-//                     initialView="dayGridMonth"
-//                     dateClick={this.handleDateClick}
-//                     editable={true}
-//                     headerToolbar={{
-//                         left: "",
-//                         center: "title",
-//                         right: ""
-//                     }}
-//                     events={[
-//                         { title: '', date: '2021-01-18' },
-//                         { title: '', date: '2021-01-21' },
-//                     ]}
-//                 />
-//             </div>
-//         )
-//     }
-// }
-
-
-// export default Calendar
